bb3610d0d4071367b18924004ad1628f72019dac
phill <phillip.rein@outlook.com>
2025-06-12 23:36:46 +0200

    completed testing of EventHanlder class

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+''' tests the functions in main'''
 import pytest
 import pygame
 import games.main as m
@@ -5,7 +6,7 @@ import games.main as m
 
 class TestInputController:
     ''' tests methods in InputController'''
-    @fixture
+    @pytest.fixture
     def get_input_controller(self):
         ''' returns an input controller'''
         class SpyInputController(m.InputController):
@@ -18,7 +19,6 @@ class TestInputController:
 
             @property
             def quits(self):
-                self.num_esc_calls += 1
                 return self._quits
 
             @quits.setter
@@ -28,7 +28,6 @@ class TestInputController:
 
             @property
             def turns_left(self):
-                self.num_left_calls += 1
                 return self._turns_left
 
             @turns_left.setter
@@ -38,7 +37,6 @@ class TestInputController:
 
             @property
             def turns_right(self):
-                self.num_left_calls += 1
                 return self._turns_right
 
             @turns_right.setter
@@ -48,53 +46,106 @@ class TestInputController:
 
         return SpyInputController()
 
+    def return_keydown(self, pygame_key):
+        ''' returns a list of 3 keydown events'''
+        events = []
+        for _ in range(3):
+            event = pygame.event.Event(
+                type=pygame.KEYDOWN, key=pygame_key
+            )
+            events.append(event)
+        return events
+
+    def return_keyup(self, pygame_key):
+        ''' returns keyup events'''
+        events = []
+        for _ in range(3):
+            event = pygame.event.Event(type=pygame.KEYUP, key=pygame_key)
+            events.append(event)
+        return events
+
 
     def return_left_list(self):
         ''' returns 3 left keys'''
-        return [pygame.K_LEFT, pygame.K_LEFT, pygame.K_LEFT,]
+        return [self.return_keydown(pygame.K_LEFT)] * 3
 
     def return_right_list(self):
         ''' returns 3 right keys'''
-        return [pygame.K_RIGHT, pygame.K_RIGHT, pygame.K_RIGHT]
+        return [self.return_keydown(pygame.K_RIGHT)] * 3
 
     def return_escape_key(self):
         ''' returns 3 escapes'''
-        return [pygame.K_ESCAPE, pygame.K_ESCAPE, pygame.K_ESCAPE]
+        return [self.return_keydown(pygame.K_ESCAPE)] * 3
 
     def return_empty(self):
+        ''' returns an empty list'''
         return []
+
+    def return_left_keyup(self):
+        ''' returns 3 keyup k_left events'''
+        return [self.return_keyup(pygame.K_LEFT)] * 3
+
+    def return_escape_keyup(self):
+        ''' returns 3 keyup k_left events'''
+        return [self.return_keyup(pygame.K_ESCAPE)] * 3
 
 
     def test_handle_inputs_k_left(self, monkeypatch, get_input_controller):
         ''' tests if the function sets the left attribute to true'''
-        monkeypatch.setattr(pygame.event, 'get', self.return_left_list)
+        monkeypatch.setattr(pygame.event, 'get', self.return_keydown)
+        monkeypatch.setattr()
         inp_controller = get_input_controller
         inp_controller.handle_events()
         assert inp_controller.turns_left
         assert inp_controller.num_left_calls == 3
         assert inp_controller.num_right_calls == 0
-        assert inp_controller.thrusts, inp_controller.restarts == False, False
+        assert ((inp_controller.thrusts, inp_controller.restarts) 
+                == (False, False))
 
 
     def test_handle_inputs_k_right(self, monkeypatch, get_input_controller):
+        ''' chekcs if right key works'''
         monkeypatch.setattr(pygame.event, 'get', self.return_right_list)
-        inpt_controller = get_input_controller
-        inp_contoller.handle_events()
-        assert inp.controller.turns_right
+        inp_controller = get_input_controller
+        inp_controller.handle_events()
+        assert inp_controller.turns_right
         assert inp_controller.num_right_calls == 3
 
 
     def test_handle_inputs_escape(self, monkeypatch, get_input_controller):
+        ''' checks if escape sequence works'''
         monkeypatch.setattr(pygame.event, 'get', self.return_escape_key)
         get_input_controller.handle_events()
         assert get_input_controller.quits
         assert get_input_controller.num_escape_calls == 3
-    
+
+
     def test_handle_inputs_empty(self, monkeypatch, get_input_controller):
+        ''' checks what happens if inputs are empty'''
         monkeypatch.setattr(pygame.event, 'get', self.return_empty)
         get_input_controller.handle_events()
         assert not get_input_controller.thrusts
         assert not get_input_controller.turns_left
-        assert not get_input_controller.turns_right 
+        assert not get_input_controller.turns_right
         assert not get_input_controller.quits
         assert not get_input_controller.restarts
+
+
+    def test_handle_inputs_keyup_k_left(
+            self, monkeypatch, get_input_controller
+    ):
+        ''' tests the keyup function for inputs'''
+        monkeypatch.setattr(pygame.event, 'get', self.return_left_keyup)
+        get_input_controller.handle_events()
+        assert not get_input_controller.turns_left
+        assert get_input_controller.num_left_calls == 3
+
+
+    def test_handle_inputs_keyup_k_escape(
+            self, monkeypatch, get_input_controller
+    ):
+        ''' tests the keyup function for inputs'''
+        monkeypatch.setattr(pygame.event, 'get', self.return_escape_keyup)
+        get_input_controller.handle_events()
+        assert not get_input_controller.quits
+        assert get_input_controller.quits == 3
